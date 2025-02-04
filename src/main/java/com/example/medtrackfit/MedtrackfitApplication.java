70c000b887f78484af8c1a32439aa14083cecafe
Prakash kumar <prakashkr2894@gmail.com>
2025-02-04 14:21:04 +0530

    project initiated
    by prakash

---
+++ src/main/java/com/example/medtrackfit/MedtrackfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.medtrackfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedtrackfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedtrackfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medtrackfit/MedtrackfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medtrackfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedtrackfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
