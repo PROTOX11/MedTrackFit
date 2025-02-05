6dd2430fcc22b946cfc38d1cf55eec276ccb2504
Prakash kumar <prakashkr2894@gmail.com>
2025-02-05 11:22:22 +0530

    homepage created
    by prakash

---
+++ src/main/java/com/example/medtrackfit/PageController.java
@@ -0,0 +1,13 @@
+package com.example.medtrackfit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("this is homepage");
+        return "home";
+    }
+}
