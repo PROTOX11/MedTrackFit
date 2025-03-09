b3c070937e1c21ca3f18afe42424522f3841cf30
Prakash kumar <prakashkr2894@gmail.com>
2025-03-09 17:50:45 +0530

    login error/success
    security enhanced
    
    login features
    
    error throw when wrong credentials entered
    
    logout

--- src/main/java/com/example/medtrackfit/config/SecurityConfig.java
+++ src/main/java/com/example/medtrackfit/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.example.medtrackfit.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.example.medtrackfit.services.impl.SecurityCustomUserDetailsService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Configuration
@@ -38,8 +49,43 @@ public class SecurityConfig {
         });
 
         //form default login page change related to form login 
-        httpSecurity.formLogin(Customizer.withDefaults());
-        
+        httpSecurity.formLogin(formLogin->{
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+            // formLogin.failureForwardUrl("/login?error=True");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+            //     @Override
+            //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            //             AuthenticationException exception) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+            //     }
+                
+                
+            // });
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            //     @Override
+            //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            //             Authentication authentication) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+            //     }
+                
+            // });
+            
+        });
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(logoutForm->{
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+
+
         return httpSecurity.build();
     }
     
